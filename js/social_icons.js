7912b0f3a01d610b69bca301a7b24cfc557b2fbd
Motta <lauropmotta@gmail.com>
2023-05-11 10:40:15 -0300

    social icons

---
+++ js/social_icons.js
@@ -0,0 +1,9 @@
+const facebook = document.querySelector('.facebook');
+const twitter = document.querySelector('.twitter');
+const instagram = document.querySelector('.instagram');
+const youtube = document.querySelector('.youtube');
+
+facebook.style.setProperty('--glow-color', '#3b5998');
+twitter.style.setProperty('--glow-color', '#1da1f2');
+instagram.style.setProperty('--glow-color', '#e1306c');
+youtube.style.setProperty('--glow-color', '#c4302b');
