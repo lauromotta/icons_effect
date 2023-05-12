044a92d5854a641c477ca6674b86415f9e8bed5c
Motta <lauropmotta@gmail.com>
2023-05-12 08:16:01 -0300

    new icon effects

---
+++ js/events.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    // Seleciona o elemento "body"
+    const body = document.querySelector('body');
+   
+    // Exibe o conteúdo do elemento "body"
+    body.style.display = 'block';
+ });
